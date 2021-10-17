086d0b2bfd906b7a18af852d9cba98ac3c158526
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-18 01:23:13 +0600

    print

---
+++ python/into.py
@@ -0,0 +1,16 @@
+""" Print """
+
+print("Bismillah")
+
+print("We are the {} who say '{}!' ".format('Knight', 'Ni'))
+print('{1} and {0}'.format('spam', 'eggs'))
+
+name = "Shahriar"
+age = 25
+
+print(f"Name {name} age {age}")
+
+print(1, 2, 3, 4 + 6, sep=',', end="\n\n")
+print(1, 2, 3, 4 + 6, sep=',',)
+
+print('-' * 50, name)
