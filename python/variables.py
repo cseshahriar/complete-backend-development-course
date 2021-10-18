e9a0a614742ed276638a45cfdac6fc5939d5ba09
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-18 19:36:01 +0600

    var

---
+++ python/variables.py
@@ -0,0 +1,24 @@
+""" Variables """
+name = "Shahriar"
+age = 25
+education = "CSE"
+
+print(f"Name: {name}, age: {age}, education: {education}")
+print(type(name))
+
+# Rules and naming convention
+"""
+snake_case *
+MACRO_CASE
+camelCase
+CapWord 
+"""
+
+PI = 3.14
+print(PI)
+
+name = "Salpin"
+print(name)
+
+age = "25"
+print(type(int(age)))
