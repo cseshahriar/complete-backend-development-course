e02a7013dffb00335af27e3a9505390022dc0e24
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-18 20:13:56 +0600

    input

---
+++ python/my_input.py
@@ -0,0 +1,11 @@
+""" Input """
+name = input("Please enter your name: ")
+print(name, type(name))
+
+num1 = int(input("Num1 "))
+num2 = int(input("Num2 "))
+
+add = num1 + num2
+sub = num1 - num2
+
+print(add)
