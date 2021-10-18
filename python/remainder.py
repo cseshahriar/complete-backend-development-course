ea5c0ce170d6787ed37ef2f2e7289803b3705fe6
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-18 20:08:38 +0600

    remainder

---
+++ python/remainder.py
@@ -0,0 +1,13 @@
+""" Remainder """
+
+int_div = 10 // 3
+print(int_div)
+
+remainder = 13 % 5  # 5*2=10+3=13
+print(remainder)
+
+num = 4
+if num % 2 == 0:
+    print('Event number')
+else:
+    print('Odd Number')
