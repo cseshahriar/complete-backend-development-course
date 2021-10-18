6e9ded48a051fe607fbd3218df2874eaeeb31b39
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-18 20:02:07 +0600

    data types

---
+++ python/data_types.py
@@ -0,0 +1,42 @@
+""" Data Types """
+
+# numeric: int, float
+
+age = 25
+PI = 3.14
+
+print("Age: ", age)
+print("PI: ", PI)
+
+# math operation
+math_operation = 1 + 2 * 3 / 4
+print(math_operation)
+print(6/4)
+
+float_div = 10 / 3
+print(float_div)
+
+int_div = 10 // 3
+print(int_div)
+
+# string
+
+"""
+multi 
+line 
+comment
+"""
+
+my_str = "Bismillah"
+print(my_str)
+
+my_str_with_quotes = 'He said "You are amazing!" parents\'s '
+print(my_str_with_quotes)
+
+name = "Shahriar"
+name += ' Hosen'
+
+print('a ' + 'b', name)
+
+my_float = 3.5
+print(my_float)
