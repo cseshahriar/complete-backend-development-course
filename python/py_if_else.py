9268ad015d374985bd5f943c531e8d37a19c3d4a
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-19 22:39:06 +0600

    even odd

--- python/py_if_else.py
+++ python/py_if_else.py
@@ -9,3 +9,12 @@ elif b >= a:
     print("b is greater than of equal a")
 else:
     print("b is less than a")
+
+
+
+#  example
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} is even number.")
+    else:
+        print(f"{i} is odd number.")
