f1467a1d1cd778fd6d47364968ec878807927cf3
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-19 22:24:48 +0600

    if elif else

---
+++ python/py_if_else.py
@@ -0,0 +1,11 @@
+""" Conditional Statements """
+
+a = 33
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif b >= a:
+    print("b is greater than of equal a")
+else:
+    print("b is less than a")
