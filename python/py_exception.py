4e0797d697d5a66198fd9b64f9a115957967148d
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-20 15:42:08 +0600

    exception

--- python/py_exception.py
+++ python/py_exception.py
@@ -36,10 +36,30 @@ except NameError as e:
 except Exception as e:
     print(e)
 
+# try:
+#   print(x)
+# except:
+#   print("Something went wrong")
+# finally:
+#     raise Exception("The 'try except' is finished")
+
+
+# key error
+def show_movie(movie):
+    try:
+        # print(f"name : {movie[0]}")
+        print(f"name : {movie[1]}")
+    except KeyError as e:
+        pass
+
+
+show_movie(['shahriar', 3])
+
+
+# attr errors
+friends3 = {'name': 'Shhariar'}
 
 try:
-  print(x)
-except:
-  print("Something went wrong")
-finally:
-    raise Exception("The 'try except' is finished")
+    print(friends3.get('age'))
+except AttributeError as e:
+    pass
