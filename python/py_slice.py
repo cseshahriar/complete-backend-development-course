31ad88840db717835a92d20c4f48ffd9cea41db7
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-20 15:16:18 +0600

    slice

---
+++ python/py_slice.py
@@ -0,0 +1,14 @@
+""" slice """
+friends = ['A', 'b', 'c', 'd']
+#          0     1    2    3
+
+print(friends[2:4])  # start, stop
+print(friends[1:])  # from starting
+
+friends2 = ['A', 'b', 'c', 'd', 'e', 'f']
+print(friends2[:4])  # stop position
+
+print(friends[:])  # new list or copy
+print(friends[:-1])  # last
+
+print(friends[-1])  # last
