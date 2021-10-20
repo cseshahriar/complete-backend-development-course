e56a7dbf4ebeab22439236ec9925c8ba0326ab85
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-20 16:13:44 +0600

    json read write

--- python/py_json.py
+++ python/py_json.py
@@ -21,3 +21,29 @@ print(m, type(m))
 
 n = json.dumps(m)
 print(n, type(n))
+
+
+print("\n")
+# read from file
+with open("person.json") as json_file:
+    data = json.load(json_file)
+
+print(data, type(data))
+
+
+print("\n")
+# write to json file
+person_dict = {
+   "name": "Bob",
+   "languages": ["English", "Fench"],
+   "married": True,
+   "age": 32
+}
+
+with open("person2.text", 'w') as json_file:
+    json.dump(person_dict, json_file)
+
+with open("person2.text") as json_file:
+    data2 = json.load(json_file)
+
+print(data, type(data2))
