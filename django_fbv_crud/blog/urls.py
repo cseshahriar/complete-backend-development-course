669fbdcb5d970aa1da84cca299f17bb350aaab36
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-10-21 22:46:18 +0600

    post update

--- django_fbv_crud/blog/urls.py
+++ django_fbv_crud/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import post_list, post_create
+from .views import post_list, post_create, post_update
 
 urlpatterns = [
     path('list/', post_list, name='post_list'),
-    path('create/', post_create, name='post_creatge'),
+    path('create/', post_create, name='post_create'),
+    path('<int:pk>/update/', post_update, name='post_update'),
 ]
--- django_fbv_crud/blog/views.py
+++ django_fbv_crud/blog/views.py
@@ -16,7 +16,8 @@ def post_create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         context = {
-            'form': form
+            'form': form,
+             'is_update': False
         }
         if form.is_valid():
             form.save()
@@ -27,6 +28,35 @@ def post_create(request):
     else:
         form = PostForm(request.POST or None)
         context = {
-            'form': form
+            'form': form,
+             'is_update': False
+        }
+        return render(request, 'create.html', context)
+
+
+def post_update(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        messages.success(request, 'Post not found 404!')
+        return redirect('post_list')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        context = {
+            'form': form,
+            'is_update': True
+        }
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Post update successfully.')
+            return redirect('post_list')
+        else:
+            return render(request, 'create.html', context)
+    else:
+        form = PostForm(request.POST or None, instance=post)
+        context = {
+            'form': form,
+            'is_update': True,
         }
         return render(request, 'create.html', context)
