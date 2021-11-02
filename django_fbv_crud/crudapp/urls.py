abb6100079a347283b34543c87c3fd05163199c3
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-11-02 09:01:17 +0600

    pagination fix

--- django_fbv_crud/crudapp/urls.py
+++ django_fbv_crud/crudapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog.views import post_list
 
 urlpatterns = [
+    path('', post_list, name='post_list'),
     path('admin/', admin.site.urls),
     path('posts/', include('blog.urls')),
 ]
